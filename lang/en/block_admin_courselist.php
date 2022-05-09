d2263de63a9c50cd26e059f16a77417e92b09817
mnm-54 <shahriarmun54@gmail.com>
2022-05-09 21:32:29 +0600

    language file

---
+++ lang/en/block_admin_courselist.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Language file
+ *
+ * @package    block_admin_courselist
+ * @copyright  2022 munem
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['admin_courselist:addinstance'] = 'Add a new admin_courselist';
+$string['admin_courselist:myaddinstance'] = 'Add a new admin_courselist to Dashboard';
+$string['admin_courselist'] = '(new admin_courselist)';
+$string['pluginname'] = 'admin courselist';
+
+
+$string['showcourses'] = 'Show courses';
